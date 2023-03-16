093d6a82db3b8a77defc48998a49635bef705846
Eric Robsky Huntley <ehuntley@mit.edu>
2023-03-16 11:20:15 -0400

    vertical lines first stab
    (not quite working - lines are not drawing correctly)

--- unknown_pleasures.R
+++ unknown_pleasures.R
@@ -1,41 +1,40 @@
-get_axes <- function(type = "horizontal") {
-  #' Get information identifying orientation of lines.
-  #'
-  #' @param type String indicating direction of lines---options are "horizontal" and "vertical"
-  #' @returns A list of axes.
-  #' @export
-  if (type == "horizontal") {
-    list("int_dim" = "y", "edge_dim" = "x")
-  } else if (type == "vertical") {
-    list("int_dim" = "x", "edge_dim" = "y")
-  } else {
-    stop("Invalid line type - expects horizontal or vertical.")
-  }
-}
-
-get_interval <- function(bbox, n, type = "horizontal") {
-  #' Generate list of interval-relevant values.
+get_dims <- function(sdf, n, type = "horizontal") {
+  #' Generate list of dimension-related quantities.
   #'
   #' @param bbox Object of type `bbox`.
   #' @param n Number of regularly-spaced lines to generate.
   #' @param type String indicating direction of lines---options are "horizontal" and "vertical".
   #' @returns A list of interval-relevant values.
   #' @export
-  axes <- get_axes(type)
+  if (type == "horizontal") {
+    axes <- list("int_dim" = "y", "edge_dim" = "x")
+  } else if (type == "vertical") {
+    axes <- list("int_dim" = "x", "edge_dim" = "y")
+  } else {
+    stop("Invalid line type - expects horizontal or vertical.")
+  }
+  bbox <- sf::st_bbox(sdf)
   int_min <- unname(bbox[paste0(axes$int_dim, "min")])
   int_max <- unname(bbox[paste0(axes$int_dim, "max")])
+  edge_min <- unname(bbox[paste0(axes$edge_dim, "min")])
+  edge_max <- unname(bbox[paste0(axes$edge_dim, "max")])
   list(
+    "axes" = axes,
     "int_min" = int_min, 
     "int_max" = int_max, 
-    "interval" = (int_max - int_min) / (n - 1)
+    "edge_min" = edge_min, 
+    "edge_max" = edge_max, 
+    "interval" = (int_max - int_min) / (n - 1),
+    "n" = n,
+    "type" = type
     )
 }
 
-st_regular_lines <- function(df, n, mask = TRUE, type = "horizontal") {
+st_regular_lines <- function(df, dims, mask = TRUE) {
   #' Generate n regularly-spaced lines over `df` extent.
   #'
   #' @param df An `sf` dataframe.
-  #' @param n Number of regularly-spaced lines to generate.
+  #' @param dims Object returned by `get_dims()`.
   #' @param mask Whether resulting lines should be clipped to df extent.
   #' @param type String indicating direction of lines---options are "horizontal" and "vertical".
   #' @returns A list of interval-relevant values.
@@ -43,21 +42,23 @@ st_regular_lines <- function(df, n, mask = TRUE, type = "horizontal") {
   print("Generating regularly spaced lines over input bbox...")
   bbox <- df %>%
     sf::st_bbox()
-  axes <- get_axes(type)
-  intervals <- get_interval(bbox, n = n)
-  edge_min <- unname(bbox[paste0(axes$edge_dim, "min")])
-  edge_max <- unname(bbox[paste0(axes$edge_dim, "max")])
   line_positions <- seq(
-      from = intervals$int_min, 
-      to = intervals$int_max, 
-      by = intervals$interval
+      from = dims$int_min, 
+      to = dims$int_max, 
+      by = dims$interval
     )
   lines <- sf::st_sfc(crs = sf::st_crs(df))
   for (line in line_positions) {
+    if (dims$type == "vertical") {
+      coords <- c(line, dims$edge_max, line, dims$edge_min)
+    } else if (dims$type == "horizontal") {
+      coords <- c(dims$edge_min, line, dims$edge_max, line)
+    }
+      
     line_const <- sf::st_sfc(
       sf::st_linestring(
         matrix(
-          c(edge_min, line, edge_max, line), 
+          coords, 
           ncol = 2, 
           byrow = TRUE
           )
@@ -69,7 +70,7 @@ st_regular_lines <- function(df, n, mask = TRUE, type = "horizontal") {
         crs = sf::st_crs(df)
     )
   }
-  sf <- sf::st_as_sf(data.frame(id = 1:n, geometry = lines)) 
+  sf <- sf::st_as_sf(data.frame(id = 1:dims$n, geometry = lines)) 
   if (mask) {
     print("Clipping lines to input layer extent...")
     sf %>%
@@ -92,14 +93,15 @@ st_regular_lines <- function(df, n, mask = TRUE, type = "horizontal") {
 st_unknown_pleasures <- function(
     lines, 
     raster, 
-    n,
+    dims,
+    max = FALSE,
     sample_size = 250,
     bleed_factor = 1.5,
     polygon = TRUE) {
   #' Transforms regularly-spaced lines into an "Unknown Pleasures"-esque set of regular section cuts based on raster value.
   #'
   #' @param lines A spatial dataframe containing regularly spaced lines.
-  #' @param n Number of regularly-spaced lines to generate.
+  #' @param dims 
   #' @param sample_size Interval, in meters, to sample along lines.
   #' @param bleed_factor How much should maxima bleed into the area of the line above or below.
   #' @param polygon If `TRUE`, outputs polygons. If `FALSE`, outputs lines.
@@ -117,9 +119,10 @@ st_unknown_pleasures <- function(
     sf::st_cast("POINT") 
   print("Extracting elevations at sample points...")
   elevated_lines$elev <- raster::extract(raster, elevated_lines)
-  max <- max(abs(elevated_lines$elev), na.rm = TRUE)
-  ints <- get_interval(sf::st_bbox(elevated_lines), n = n)
-  scale <- ints$interval / max
+  if (!max) {
+    max <- max(abs(elevated_lines$elev), na.rm = TRUE)
+  }
+  scale <- dims$interval / max
   print("Performing Affine Transform on points...")
   elevated_lines <- elevated_lines %>%
     tidyr::drop_na(elev) %>%
@@ -131,7 +134,11 @@ st_unknown_pleasures <- function(
     ) %>%
     dplyr::rowwise() %>%
     dplyr::mutate(
-      geometry = geometry + c(0, elev_scaled)
+      geometry = ifelse(
+        (dims$type == "horizontal"),
+        geometry + c(0, elev_scaled),
+        geometry + c(elev_scaled, 0)
+        )
     ) %>%
     dplyr::ungroup() %>%
     sf::st_set_crs(sf::st_crs(lines)) %>%
